90c4b574ac51b8b9c1335946ea73621ff1bf8f74
Priit Rätsep <pratsep@itcollege.ee>
2016-10-01 18:33:15 +0300

    hello world

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by ratse on 01.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
