a92f14898436ae4f040d9e6c895d9f06a6f700ec
pratsep <pratsep@itcollege.ee>
2016-10-01 19:20:53 +0300

    Update HelloWorld.java

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -6,5 +6,6 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HELLO World!");
+        System.out.println("Minu nimi on Priit!");
     }
 }
