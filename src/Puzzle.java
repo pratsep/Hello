a7ffa65ee58f5b195561f0e4c78c6a77936f7300
Priit Rätsep <pratsep@itcollege.ee>
2016-10-11 23:05:35 +0300

    Puzzle lisamine kus tegelt on HelloWorld

---
+++ src/Puzzle.java
@@ -0,0 +1,6 @@
+public class Puzzle {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Tegelikult tuleb siia minu mäng");
+    }
+}
