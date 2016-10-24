021dd680f1aa11ae4e4b5cd0e891cc1ddc5436b4
Priit Rätsep <pratsep@itcollege.ee>
2016-10-24 20:11:33 +0300

    Teine vaheetapp, javaFX proov

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,7 +1,21 @@
-public class Puzzle {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Tegelikult tuleb siia minu mäng");
-        System.out.println("Lebo?");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Puzzle extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane alus = new Pane();
+        Scene stseen = new Scene(alus, 500, 500);
+        primaryStage.setScene(stseen);
+        primaryStage.setTitle("Proovikas");
+        Rectangle ruut = new Rectangle(150, 150, 200, 200);
+        alus.getChildren().add(ruut);
+        ruut.setFill(Color.YELLOWGREEN);
+        ruut.setStroke(Color.RED);
+        primaryStage.show();
     }
 }
