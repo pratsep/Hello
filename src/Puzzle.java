b422ad65384498c6d628c8313ede90b43b323dcb
Priit Rätsep <pratsep@itcollege.ee>
2016-12-29 14:05:50 +0200

    raskusastmete valiku koodi lyhendamine

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,6 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
@@ -21,15 +21,45 @@ public class Puzzle extends Application {
     //String ID1;
     //String ID2;
     int lvl1Pildid = new File("src/images").listFiles().length;
-    int juppe = 9;
+    int juppe;
     ArrayList<ImageView> list = new ArrayList();
     ArrayList<ImageView> kontroll = new ArrayList();
     @Override
     public void start(Stage primaryStage) throws Exception {
-        looPuzzle();
-        klikk1();
-        kontrollimine();
+        algus();
     }
+    private void algus() {
+        Stage alustame = new Stage();
+        VBox algusala = new VBox();
+        Scene algusstseen = new Scene(algusala, 500, 500);
+        alustame.setScene(algusstseen);
+        alustame.show();
+        Button start = new Button("Mängime");
+        start.setLayoutX(2000);
+        ToggleGroup g = new ToggleGroup();
+        RadioButton lihtne = new RadioButton("(2x2)");
+        lihtne.setUserData(4);
+        RadioButton keskmine = new RadioButton("(3x3)");
+        keskmine.setUserData(9);
+        RadioButton raske = new RadioButton("(4x4");
+        raske.setUserData(16);
+        lihtne.setToggleGroup(g);
+        keskmine.setToggleGroup(g);
+        raske.setToggleGroup(g);
+        lihtne.setSelected(true);
+        Label raskusastmed = new Label("Raskusastmed");
+        algusala.getChildren().addAll(start, raskusastmed, lihtne, keskmine, raske);
+        start.setOnAction(event -> {
+            juppe =  (int)(g.getSelectedToggle().getUserData());
+            System.out.println(juppe);
+            looPuzzle(juppe);
+            klikk1();
+            kontrollimine();
+            alustame.close();
+        }
+        );
+    }
+
     private void kontrollimine() {
         int asend = 0;
         for (int i = 0; i < juppe; i++) {
@@ -128,7 +158,8 @@ public class Puzzle extends Application {
             }
         });
     }
-    private void looPuzzle() {
+
+    private void looPuzzle(int juppe) {
         Scene x3 = new Scene(ruudustik, 600, 600);
         Stage puzzleAla = new Stage();
         puzzleAla.setScene(x3);
@@ -141,12 +172,22 @@ public class Puzzle extends Application {
 
         //Pildi lõikamine ja lisamine ArrayListi
         int IDlugeja = 1;
+        Double kordaja=0.0;
+        if (juppe == 4){
+            kordaja = 1.5;
+        }
+        else if (juppe == 9){
+            kordaja = 1.0;
+        }
+        else if (juppe == 16){
+            kordaja = 0.75;
+        }
         for (int i = 0; i < Math.sqrt(juppe); i++) {
             for (int j = 0; j < Math.sqrt(juppe) ; j++) {
                 Image origPilt = new Image("images/" + rngPilt + ".png");
                 ImageView imv = new ImageView();
                 PixelReader piltSisse = origPilt.getPixelReader();
-                WritableImage uusPilt = new WritableImage(piltSisse, j*200, i*200, 200, 200);
+                WritableImage uusPilt = new WritableImage(piltSisse, (int)(j*200*kordaja), (int)(i*200*kordaja), (int)(200*kordaja), (int)(200*kordaja));
                 imv.setImage(uusPilt);
                 imv.setId(String.valueOf(IDlugeja));
                 list.add(imv);
@@ -174,7 +215,9 @@ public class Puzzle extends Application {
                 double kraad = ((int)(Math.random()*4))*90;
                 pilt.setRotate(kraad);
                 z = z+1;
+
             }
         }
+        System.out.println(ruudustik.getChildren());
     }
 }
